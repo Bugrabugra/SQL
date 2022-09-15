e669c38a8b1f6cb7b7b414baf4abd6427e155875
Bugrabugra <bugraotken@gmail.com>
2022-09-16 02:12:35 +0300

    budgetron

--- Projeler/Budgetron/Queries.sql
+++ Projeler/Budgetron/Queries.sql
@@ -1,25 +1,82 @@
 -- year
 SELECT *
-FROM FinancialStatus
-WHERE strftime('%Y', date) = '2022';
+FROM
+  FinancialStatus
+WHERE
+  STRFTIME('%Y', date) = '2022';
 
 -- year, month
 SELECT *
-FROM FinancialStatus
-WHERE strftime('%Y-%m', date) = '2022-08';
+FROM
+  FinancialStatus
+WHERE
+  STRFTIME('%Y-%m', date) = '2022-08';
 
 -- year, month, day
 SELECT *
-FROM FinancialStatus
-WHERE strftime('%Y-%m-%d', date) = '2022-09-04';
+FROM
+  FinancialStatus
+WHERE
+  STRFTIME('%Y-%m-%d', date) = '2022-09-04';
 
--- year
-SELECT strftime('%Y-%m', date), SUM(value), value_type
-FROM FinancialStatus
-WHERE strftime('%Y', date) = '2022'
-GROUP BY strftime('%Y-%m', date), value_type
+-- group by
+SELECT
+    (SELECT '2022-09')       AS date,
+  (SELECT SUM(value)
+   FROM
+     FinancialStatus
+   WHERE
+     STRFTIME('%Y-%m', date) = '2022-09' AND
+     value_type = 'income')  AS 'income',
+  (SELECT SUM(value)
+   FROM
+     FinancialStatus
+   WHERE
+     STRFTIME('%Y-%m', date) = '2022-09' AND
+     value_type = 'expense') AS 'expense'
+FROM
+  FinancialStatus
+WHERE
+  STRFTIME('%Y-%m', date) = '2022-09'
+GROUP BY
+  STRFTIME('%Y-%m', date)
 ;
 
+-- select specific
+SELECT *
+FROM
+  FinancialStatus
+WHERE
+  STRFTIME('%Y-%m-%d', date) = '2022-07-04';
 
+-- select detailed
+SELECT
+    (SELECT d.dates)         AS date,
+  (SELECT SUM(value)
+   FROM
+     FinancialStatus
+   WHERE
+     STRFTIME('%Y-%m-%d', date) = d.dates AND
+     value_type = 'income')  AS 'income',
+  (SELECT SUM(value)
+   FROM
+     FinancialStatus
+   WHERE
+     STRFTIME('%Y-%m-%d', date) = d.dates AND
+     value_type = 'expense') AS 'expense'
+
+FROM
+  FinancialStatus AS fs,
+  (SELECT DISTINCT
+     STRFTIME('%Y-%m-%d', date) AS dates
+   FROM
+     FinancialStatus
+   WHERE
+     STRFTIME('%Y-%m', date) = ?)
+    AS d
+WHERE
+  STRFTIME('%Y-%m-%d', date) = d.dates
+GROUP BY
+  STRFTIME('%Y-%m-%d', date)
 
 
