758b1e6196a9aeebf735ad4b0668cb1c4e12fdd8
Buğra <bugraotken@gmail.com>
2022-11-11 17:56:16 +0300

    created spatial queries for TrioMobil

--- Projeler/TrioMobil/spatial queries/queries.sql
+++ Projeler/TrioMobil/spatial queries/queries.sql
@@ -1,49 +1,77 @@
 -- select neighborhoods containing the vehicles
+SELECT
+  neighborhoods.id, neighborhoods.adi, neighborhoods.ilceadi, neighborhoods.iladi
+FROM
+  neighborhoods
+WHERE
+  st_intersects(
+      neighborhoods.geom,
+      st_collect(
+  ARRAY(SELECT
+     ST_SetSRID(st_makepoint(st_x(vehicles.geom), st_y(vehicles.geom)), 4326) as coordinates
+   FROM
+     vehicles
+   WHERE
+     id IN (200, 400, 600, 800, 1000, 1200, 1400))));
+
+--select vehicles inside a neighborhood
+SELECT id
+FROM
+  vehicles
+WHERE
+  st_contains(
+      (SELECT geom
+       FROM
+         neighborhoods
+       WHERE
+         iladi = 'İstanbul' AND
+         ilceadi = 'Maltepe' AND
+         adi = 'İdealtepe'),
+      vehicles.geom
+    );
+
+--select vehicles inside a district
+SELECT id
+FROM
+  vehicles
+WHERE
+  st_contains(
+      (SELECT geom
+       FROM
+         districts
+       WHERE
+         districts.iladi = 'İstanbul' AND
+         districts.ilceadi = 'Maltepe'),
+      vehicles.geom
+    );
+
+--select vehicles inside a city
+SELECT id
+FROM
+  vehicles
+WHERE
+  st_contains(
+      (SELECT geom
+       FROM
+         cities
+       WHERE
+         cities.iladi = 'İstanbul'),
+      vehicles.geom
+    );
+
+SELECT id
+FROM
+  vehicles
+WHERE
+  id % 2 = 0;
+
 select *
 from neighborhoods
 where st_intersects(
     geom,
-    ST_SetSRID(st_makepoint(33.1064,38.7650),4326)
+    st_collect(
+        ST_SetSRID(st_makepoint(33.1064,38.7650),4326),
+        ST_SetSRID(st_makepoint(28.98843,41.12492),4326)
+      )
   );
 
---select vehicles inside a neighborhood
-select id
-from vehicles
-where st_contains(
-    (
-      select geom
-      from neighborhoods
-      where
-          iladi = 'İstanbul' and
-          ilceadi = 'Maltepe' and
-          adi = 'İdealtepe'
-    ),
-    vehicles.geom
-  );
-
---select vehicles inside a district
-select id
-from vehicles
-where st_contains(
-    (
-      select geom
-      from districts
-      where
-          districts.iladi = 'İstanbul' and
-          districts.ilceadi = 'Maltepe'
-    ),
-    vehicles.geom
-  );
-
---select vehicles inside a city
-select id
-from vehicles
-where st_contains(
-    (
-      select geom
-      from cities
-      where
-          cities.iladi = 'İstanbul'
-    ),
-    vehicles.geom
-  );
